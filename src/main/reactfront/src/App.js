17d69d02e6b62d9ea2bc4f348125c305459992bb
Landsoft_EpsteinKim <yskim9718@naver.com>
2022-06-13 18:53:03 +0900

    Axios 찍먹(완료) Thrid Party Router 설정 진행중

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,18 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [hello, setHello] = useState('');
+  const [hello, setHello] = useState("");
 
   useEffect(() => {
-    axios.get('/api/hello')
-      .then(response => setHello(response.data))
-      .catch(error => console.log(error))
+    axios
+      .get("/api/hello")
+      .then((response) => setHello(response.data))
+      .catch((error) => console.log(error));
   }, []);
+
+  //임시
+  const RoutesData = () => (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+
+  const AxiosData = () => (
+    <div>백엔드에서 가져온 데이터입니다 : {hello}</div>
+  );
 
   return (
     <div>
-      백엔드에서 가져온 데이터입니다 : {hello}
+      <AxiosData />
     </div>
   );
 }
